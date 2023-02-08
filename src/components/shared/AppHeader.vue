365038185958d633781b5056ef47bcb99ff2a388
Tim Hunter <thunte27@uwo.ca>
2023-02-08 17:49:08 -0500

    add rudimentary logo for tim hunter dev

--- src/components/shared/AppHeader.vue
+++ src/components/shared/AppHeader.vue
@@ -87,14 +87,14 @@ export default {
 					<router-link to="/"
 						><img
 							v-if="theme === 'light'"
-							src="@/assets/images/logo-dark.svg"
-							class="w-36"
+							src="@/assets/images/timhunter-logo-dark.svg"
+							class="w-72"
 							alt="Dark Logo"
 						/>
 						<img
 							v-else
-							src="@/assets/images/logo-light.svg"
-							class="w-36"
+							src="@/assets/images/timhunter-logo-light.svg"
+							class="w-72"
 							alt="Light Logo"
 						/>
 					</router-link>
