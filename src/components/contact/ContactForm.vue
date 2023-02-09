525f9b72beb58a77c93199cb3e3b1658d81b6b9f
Tim Hunter <thunte27@uwo.ca>
2023-02-08 21:09:47 -0500

    feat: add emailjs backend to contact form

--- src/components/contact/ContactForm.vue
+++ src/components/contact/ContactForm.vue
@@ -2,40 +2,51 @@
 import Button from '../reusable/Button.vue';
 import FormInput from '../reusable/FormInput.vue';
 import FormTextarea from '../reusable/FormTextarea.vue';
-export default { components: { Button, FormInput, FormTextarea } };
+
+import emailjs from '@emailjs/browser';
+
+
+console.log(process.env);
+
+export default {
+	components: { Button, FormInput, FormTextarea },
+	methods: {
+		sendEmail() {
+			console.log('Sending email...');
+			emailjs.sendForm(process.env.VUE_APP_EMAILJS_SERVICE_ID, process.env.VUE_APP_EMAILJS_TEMPLATE_ID, this.$refs.form, process.env.VUE_APP_EMAILJS_PUBLIC_KEY)
+				.then((result) => {
+					console.log('SUCCESS!', result.text);
+				}, (error) => {
+					console.log('FAILED...', error.text);
+				});
+		}
+	}
+};
 </script>
 
 <template>
 	<div class="w-full md:w-1/2">
 		<div
-			class="leading-loose max-w-xl m-4 p-7 bg-secondary-light dark:bg-secondary-dark rounded-xl shadow-xl text-left"
-		>
-			<p
-				class="font-general-medium text-primary-dark dark:text-primary-light text-2xl mb-8"
-			>
+			class="leading-loose max-w-xl m-4 p-7 bg-secondary-light dark:bg-secondary-dark rounded-xl shadow-xl text-left">
+			<p class="font-general-medium text-primary-dark dark:text-primary-light text-2xl mb-8">
 				Contact Form
 			</p>
-			<form action="#" class="font-general-regular space-y-7">
+			<form ref="form" class="font-general-regular space-y-7" @submit.prevent="sendEmail">
 				<FormInput label="Full Name" inputIdentifier="name" />
-				<FormInput
-					label="Email"
-					inputIdentifier="email"
-					inputType="email"
-				/>
+				<FormInput label="Email" inputIdentifier="email" inputType="email" />
 				<FormInput label="Subject" inputIdentifier="subject" />
 				<FormTextarea label="Message" textareaIdentifier="message" />
 
 				<div>
-					<Button
-						title="Send Message"
+					<Button title="Send Message"
 						class="px-4 py-2.5 text-white tracking-wider bg-emerald-500 hover:bg-emerald-600 focus:ring-1 focus:ring-emerald-900 rounded-lg duration-500"
-						type="submit"
-						aria-label="Send Message"
-					/>
+						type="submit" aria-label="Send Message" />
 				</div>
 			</form>
 		</div>
 	</div>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+
+</style>
