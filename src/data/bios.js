6375f609a5720d97117f2462acd33348d46240da
Tim Hunter <thunte27@uwo.ca>
2023-03-03 23:10:45 -0500

    fix: bio content

--- src/data/bios.js
+++ src/data/bios.js
@@ -2,12 +2,7 @@ const bios = [
     {
         id: 1,
         bio:
-        'I am a recent M.Sc. graduate from University of Western Ontario, I’m applying for the position of Python Developer. Through my training as a computational researcher, my recent Data Science accreditations from John’s Hopkins University, as well as various personal passion projects, I have acquired many skills which uniquely suit me to the position of Python Developer at Morgan Stanley.',
-    },
-    {
-        id: 2,
-        bio:
-            'I am looking to advance my career as a data analyst. I have a strong background in data analysis and data visualization. I am a quick learner and I am always looking to improve my skills.',
+        'M.Sc. graduate trained in computational medicine. Proficient in programming for data science and visualization. Excellent written and oral communication skills. Enthusiastic about learning new technologies and using data to understand complex issues.',
     },
 ]
 
